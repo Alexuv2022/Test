a93e8f00cd4eb28b3ebaf31e915512bb3f45bcb5
Alexuv2022 <01052020>
2022-07-24 17:19:32 +0300

    Обработка ip с помощью нескольких потоков

---
+++ src/lesson15/dz/Main2.java
@@ -0,0 +1,84 @@
+package lesson15.dz;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        try {
+            FileInputStream fin = new FileInputStream("C://java/ip.txt");
+            int i;
+            String result = "";
+            while ((i = fin.read()) != -1) {
+                if (i == 13) {
+                    continue;
+                } else if (i == 10) {
+                    String ip = result.split(":")[0];
+                    int port = Integer.parseInt(result.split(":")[1]);
+                    MyThread thread = new MyThread(ip, port);
+                    thread.start();
+                    result = "";
+                } else if (i == 9) {
+                    result += ":";
+                } else {
+                    result += (char) i;
+                }
+
+            }
+            System.out.print(result);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void checkProxy(String ip, int port) {
+        System.out.println("«‡ÔÓÒ Í ÒÂ‚ÂÛ Ò " + ip + ":" + port);
+
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, port));
+            URL url = new URL("https://vozhzhaev.ru/test.php");
+            URLConnection connection = url.openConnection(proxy);
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            System.out.println("ip: " + ip + " port: " + port + " - –¿¡Œ“¿≈“!");
+            System.out.println((char) in.read());
+            proxyToFile(ip + ":" + port);
+        } catch (Exception e) {
+            System.out.println("ip: " + ip + " port: " + port + " - ¿””!");
+        }
+    }
+
+    public static void proxyToFile(String result) {
+        try {
+            FileOutputStream fos = new FileOutputStream("C://java/even_proxy.txt", true);
+            String text = result + "\n";
+            byte[] buffer = text.getBytes();
+            fos.write(buffer, 0, buffer.length);
+            System.out.println("The file has been written");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class MyThread extends Thread {
+    String ip;
+    int port;
+
+    public MyThread(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        Main.checkProxy(ip, port);
+    }
+}
